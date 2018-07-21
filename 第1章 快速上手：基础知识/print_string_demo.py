263ca64dea7e91dc3be0d02f143e97548002c539
守正 <707345032@qq.com>
2018-07-21 13:01:25 +0800

    print_string_demo

---
+++ 第1章 快速上手：基础知识/print_string_demo.py
@@ -0,0 +1,6 @@
+# _*_ coding:utf-8
+# 原始字符串：不以特殊方式处理反斜杠
+print(r'C:\Users\qian\Python_demo\第1章 快速上手：基础知识\n')
+print(r'C:\Users\qian\Python_demo\第1章 快速上手：基础知识' + "\\bin")
+print("let\'s go!")
+print(r"let\'s go!")
