f0d2f8556ab6888f6e02b8febef199ecb6c99160
守正 <707345032@qq.com>
2018-07-23 17:48:31 +0800

    string_method_demo

--- 第3章 使用字符串/string_method.py
+++ 第3章 使用字符串/string_method.py
@@ -1,1 +1,16 @@
-# _*_ coding:utf-8
+# _*_ coding:utf-8 _*_
+# String方法示例
+# center两端添加相同的字符串
+# test = "The Middle by Jimmy Eat world".center(39)
+# print(test)
+#
+# test = "The Middle by Jimmy Eat world".center(39, "*")
+# print(test)
+
+# find 查找
+y = 'With a moo-moo here, and a moo-moo there'
+print(y.find('he'))
+title = "Monty Python's Flying Circus"
+print(title.find('Monty'))
+print(title.find('Python'))
+print(title.find('test'))
