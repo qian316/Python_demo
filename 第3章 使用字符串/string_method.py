c7a1e892795ceed3c9a96a803ba5ff9d85437984
守正 <707345032@qq.com>
2018-07-24 10:12:11 +0800

    string_method_demo

--- 第3章 使用字符串/string_method.py
+++ 第3章 使用字符串/string_method.py
@@ -14,3 +14,26 @@ title = "Monty Python's Flying Circus"
 print(title.find('Monty'))
 print(title.find('Python'))
 print(title.find('test'))
+
+# join
+dirs = '','usr', 'bin','env'
+sep = '/'
+print(sep.join(dirs))
+
+
+# lower
+low =  'Trondheim Hammer Dance'
+print(low.lower())
+
+# relace
+rel = 'This is a test'.replace('is', 'eez')
+print(rel)
+rels = ('This is a test','This is a tist')
+print(rels[1].replace('is', 'eez'))
+# print(rels)
+
+
+# split
+print(sep.join(dirs).split('/'))
+spl ='/usr/bin/env'.split('/')
+print(spl)
