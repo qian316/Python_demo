114287373eeb9252458dbdbbeb922ff56ada969c
守正 <707345032@qq.com>
2018-07-25 17:20:26 +0800

    if_else_demo

---
+++ 第5章 条件、循环及其他语句/if_else_demo.py
@@ -0,0 +1,16 @@
+# _*_ coding:utf-8 _*_
+
+# is 相同运算符
+x = y = [1,2,3]
+z = [1,2,3]
+print(x == y)
+print(x == z )
+print(x is y)
+print(x is z )
+
+print(chr(128584))
+
+# assert
+age = -1
+assert 0 < age < 100 , "The age must be realistic."
+
