65ade75e1d6676207536d8ee38c8b9beacbfd440
守正 <707345032@qq.com>
2018-07-26 17:25:40 +0800

    循环demo练习

--- 第5章 条件、循环及其他语句/for_demo.py
+++ 第5章 条件、循环及其他语句/for_demo.py
@@ -15,3 +15,25 @@ for number in range(1,101):
 d = {'x': '1','y': '2','z': '3'}
 for key,value in d.items():
     print(key, 'corresponds to', value)
+
+# 迭代工具
+name = ['anne','beth','george','damon']
+ages = [12,23,33,44]
+
+# print(list(zip(name,ages)))
+print(zip(name,ages))
+
+# while break
+# while True:
+#     word = input('Please enter a word: ')
+#     if not word: break
+#     print('The word was ', word)
+
+from math import sqrt
+for n in range(99,81,-1):
+    root = sqrt(n)
+    if root == int(root):
+        print(n)
+        break
+else:
+    print("Didn't find it!")
