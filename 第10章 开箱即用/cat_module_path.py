1eec8266b051bbde853040219b31bce581b573b0
守正 <707345032@qq.com>
2018-09-30 21:44:28 +0800

    查看模块文档和文档位置

--- 第10章 开箱即用/cat_module_path.py
+++ 第10章 开箱即用/cat_module_path.py
@@ -2,6 +2,7 @@
 # 查看Python解释器模中模块的位置
 import  sys,pprint
 pprint.pprint(sys.path)
+import copy
 
 # import module_dmeo
 # a = module_dmeo
@@ -9,3 +10,7 @@ pprint.pprint(sys.path)
 from module_test import module_dmeo
 # import module_test.module_dmeo
 print(module_dmeo.hello())
+
+# 模块位置和模块文档
+print(copy.__file__)
+print(copy.__doc__)
