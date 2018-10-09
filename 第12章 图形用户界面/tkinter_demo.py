d49aebd919259e1eaef988a8724d9175b9ef7dfd
chengqian <chengqian@banggood.com>
2018-10-09 09:11:14 +0800

    简单的GUI示例

--- 第12章 图形用户界面/tkinter_demo.py
+++ 第12章 图形用户界面/tkinter_demo.py
@@ -1,1 +1,25 @@
 # _*_ coding:utf-8 _*_
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+
+# 简单的GUI示例
+
+def load():
+    with open(filename.get()) as file:
+        contents.delete('1.0', END)
+    contents.insert(INSERT, file.read())
+
+
+def save():
+    with open(filename.get(), 'w') as file:
+        file.write(contents.get('1.0', END))
+top = Tk()
+top.title("Simple Editor")
+contents = ScrolledText()
+contents.pack(side=BOTTOM, expand=True, fill=BOTH)
+filename = Entry()
+filename.pack(side=LEFT, expand=True, fill=X)
+Button(text='Open', command=load).pack(side=LEFT)
+Button(text='Save', command=save).pack(side=LEFT)
+mainloop()
+
